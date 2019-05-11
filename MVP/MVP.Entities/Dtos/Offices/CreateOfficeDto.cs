3b5efafd472defcc4ba110f30b100c527a8d304b
Deimantas Gedgaudas <gedgaudasdei@gmail.com>
2019-05-11 18:03:39 +0300

    Add LocationDto and CreateLocationDto.

---
+++ MVP/MVP.Entities/Dtos/Offices/CreateOfficeDto.cs
@@ -0,0 +1,22 @@
+﻿using MVP.Entities.Dtos.Locations;
+using MVP.Entities.Dtos.Office;
+
+
+namespace MVP.Entities.Dtos.Offices
+{
+    class CreateOfficeDto : OfficeDto
+    {
+        public static Entities.Office ToEntity(OfficeDto newOfficeDto)
+        {
+            var office = new Entities.Office
+            {
+                Name = newOfficeDto.Name,
+                Location = CreateLocationDto.ToEntity(newOfficeDto.LocationDto),
+                // TODO ApartmentsDto
+                //Apartments = CreateApartmentsDto.ToEntity()
+            };
+
+            return office;
+        }
+    }
+}
