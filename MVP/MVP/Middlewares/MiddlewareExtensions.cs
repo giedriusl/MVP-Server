97ff349e21b4982dbe15fa08fe952b65f3b7edeb
Justinas Šideikis <justinas.sideikis@tieto.com>
2019-05-15 12:37:25 +0200

    MVP-063: Options request middleware added

---
+++ MVP/MVP/Middlewares/MiddlewareExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace MVP.Middlewares
+{
+
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseOptions(this IApplicationBuilder builder, string policy)
+        {
+            return builder.UseMiddleware<OptionsMiddleware>(policy);
+        }
+    }
+}
---
+++ MVP/MVP/Middlewares/OptionsMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+
+namespace MVP.Middlewares
+{
+    public class OptionsMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ICorsService _corsService;
+        private readonly ICorsPolicyProvider _corsPolicyProvider;
+        private readonly string _corsPolicy;
+
+        public OptionsMiddleware(RequestDelegate next, ICorsService corsService, ICorsPolicyProvider corsPolicyProvider, string corsPolicy)
+        {
+            _next = next;
+            _corsService = corsService;
+            _corsPolicyProvider = corsPolicyProvider;
+            _corsPolicy = corsPolicy;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Headers.ContainsKey(CorsConstants.Origin))
+            {
+                var corsPolicy = await _corsPolicyProvider.GetPolicyAsync(context, "AllowAllHeaders");
+                if (corsPolicy != null)
+                {
+                    var corsResult = _corsService.EvaluatePolicy(context, corsPolicy);
+                    _corsService.ApplyResult(corsResult, context.Response);
+
+                    if (context.Request.Method == HttpMethods.Options)
+                    {
+                        context.Response.StatusCode = StatusCodes.Status204NoContent;
+                        return;
+                    }
+                }
+            }
+
+            await _next.Invoke(context);
+        }
+    }
+
+}
