5748b5e71a3c802de919972303fbcc764b8f3d1b
Deimantas Gedgaudas <gedgaudasdei@gmail.com>
2019-05-16 15:19:08 +0300

    Merging read/write interfaces into one interface. Same with the repository classes.

--- MVP/MVP.DataAccess/Interfaces/IOfficeRepository.cs
+++ MVP/MVP.DataAccess/Interfaces/IOfficeRepository.cs
@@ -4,10 +4,14 @@ using MVP.Entities.Entities;
 
 namespace MVP.DataAccess.Interfaces
 {
-    interface IOfficeRepositoryRead
+    interface IOfficeRepository
     {
         Task<Office> GetOfficeById(int officeId);
         Task<Office> GetOfficeByName(string name);
-        Task<List<Office>> GetAllOffices();
+        Task<IEnumerable<Office>> GetAllOffices();
+
+        Task AddOffice(Office office);
+        Task AddOfficeList(List<Office> offices);
+        Task UpdateOffice(Office office);
     }
 }
