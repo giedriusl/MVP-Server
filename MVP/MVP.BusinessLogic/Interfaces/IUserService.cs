bd9f7d6c689ea0321689f563711661bd16f9c362
giedriusl <giedrius.lukocius@visma.com>
2019-05-17 10:55:50 +0300

    delete RememberMe and unnecessary dto

--- MVP/MVP.BusinessLogic/Interfaces/IUserService.cs
+++ MVP/MVP.BusinessLogic/Interfaces/IUserService.cs
@@ -1,13 +1,12 @@
-﻿using System.Threading.Tasks;
-using MVP.Entities.Dtos;
-using MVP.Entities.Entities;
+﻿using MVP.Entities.Dtos;
+using System.Threading.Tasks;
 
 namespace MVP.BusinessLogic.Interfaces
 {
     public interface IUserService
     {
-        Task CreateAsync(CreateUserDto newUserDto);
-        Task<string> LoginAsync(UserLoginDto userLoginDto);
+        Task CreateAsync(CreateUserDto createUserDto);
+        Task<string> LoginAsync(UserDto userDto);
         Task ResetPassword(ResetPasswordDto resetPasswordDto);
         Task SendResetPasswordLink(string email);
     }
