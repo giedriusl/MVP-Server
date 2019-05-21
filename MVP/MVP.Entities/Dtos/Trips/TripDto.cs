c142b27b6106864cea25205b6fad5d745bf717cf
Deimantas Gedgaudas <gedgaudasdei@gmail.com>
2019-05-21 21:53:05 +0300

    Modified TripDto class. In TripService class changed TripViewDto to TripDto.

--- MVP/MVP.Entities/Dtos/Trips/TripDto.cs
+++ MVP/MVP.Entities/Dtos/Trips/TripDto.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using MVP.Entities.Dtos.FlightsInformation;
 using MVP.Entities.Dtos.RentalCarsInformation;
+using MVP.Entities.Entities;
 
 namespace MVP.Entities.Dtos.Trips
 {
@@ -25,5 +26,17 @@ namespace MVP.Entities.Dtos.Trips
         public TripStatus TripStatus { get; set; }
         public List<FlightInformationDto> FlightInformations { get; set; } = new List<FlightInformationDto>();
         public List<RentalCarInformationDto> RentalCarInformations { get; set; } = new List<RentalCarInformationDto>();
+
+
+        public static TripDto ToDto(Trip trip)
+        {
+            return new TripDto
+            {
+                Title = trip.Title,
+                Start = trip.Start,
+                End = trip.End,
+                TripStatus = trip.TripStatus
+            };
+        }
     }
 }
