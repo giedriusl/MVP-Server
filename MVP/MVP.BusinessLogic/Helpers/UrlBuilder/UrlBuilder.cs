be15f9ecd97b555cd18047aa57162939e50e5568
Justinas Šideikis <justinas.sideikis@tieto.com>
2019-05-23 20:23:12 +0300

    Reset password addapted to FE

--- MVP/MVP.BusinessLogic/Helpers/UrlBuilder/UrlBuilder.cs
+++ MVP/MVP.BusinessLogic/Helpers/UrlBuilder/UrlBuilder.cs
@@ -14,7 +14,7 @@ namespace MVP.BusinessLogic.Helpers.UrlBuilder
         public string BuildPasswordResetLink(string token, string email)
         {
             var invitationUrl = _configuration["Urls:PasswordResetUrl"];
-            var urlWithToken = invitationUrl + $"?token={token}&email={email}";
+            var urlWithToken = invitationUrl + $"/{email}/token/{token}";
 
             return urlWithToken;
         }
